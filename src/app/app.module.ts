5f91c493e2f4b6783a007e21345580d72c0d584d
Matthew Stevens <mwstevens0708@gmail.com>
2020-05-08 13:58:19 -0600

    Added repo-detail component and setup placeholder html template.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RepoDetailComponent } from './component/repo-detail/repo-detail.component';
 //import { RepoDetailComponentComponent } from './repo-detail-component/repo-detail-component.component';
 //mport { RepoDetailComponent } from './repo-detail/repo-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    RepoDetailComponent,
    // RepoDetailComponentComponent,
     //RepoDetailComponent
   ],
