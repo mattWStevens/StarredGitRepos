64f60a9e8e282f52917f5a7484112cf5ad3c54bf
mattWStevens <mwstevens0708@gmail.com>
2020-05-16 17:49:56 -0600

    Added in ActivatedRoute to use route params to display data in RepoDetailComponent.

--- src/app/components/repo-detail/repo-detail.component.ts
+++ src/app/components/repo-detail/repo-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Repo } from 'src/app/repo';
+import { RepoService } from '../../services/repo.service';
+import { PARAMETERS } from '@angular/core/src/util/decorators';
 
 @Component({
   selector: 'app-repo-detail',
@@ -6,10 +10,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./repo-detail.component.css']
 })
 export class RepoDetailComponent implements OnInit {
+  repo: Repo;
 
-  constructor() { }
+  constructor(private repoService: RepoService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    const repoList: Repo[] = this.repoService.getRepos();
+
+    this.activatedRoute.paramMap.subscribe(params => {
+      this.repo = repoList[+params.get('repoId')];
+    });
   }
 
 }
