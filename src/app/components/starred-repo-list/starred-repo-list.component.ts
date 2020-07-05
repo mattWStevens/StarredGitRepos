132585d182e76bf17e42cc3ac99fb78d4659b4f0
mattWStevens <gibsonhistoric@gmail.com>
2020-07-05 16:03:39 -0600

    Correctly get observable and map it to array. Now just need to replace current implementation with the observable response as opposed to hardcoded RepoService.

--- src/app/components/starred-repo-list/starred-repo-list.component.ts
+++ src/app/components/starred-repo-list/starred-repo-list.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { RepoService } from '../../services/repo.service';
 import { Repo } from '../../repo';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { RepoResponse } from 'src/app/repo-response';
 
 @Component({
   selector: 'app-starred-repo-list',
@@ -10,10 +13,19 @@ import { Repo } from '../../repo';
 })
 export class StarredRepoListComponent implements OnInit {
   repos: Repo[];
+  realRepos$: Observable<RepoResponse[]>;
 
-  constructor(private router: Router, private repoService: RepoService) { }
+  constructor(private router: Router, private repoService: RepoService, private http: HttpClient) { }
 
   ngOnInit() {
     this.repos = this.repoService.getRepos();
+    this.realRepos$ = this.http.get<RepoResponse[]>('https://api.github.com/users/mattWStevens/starred')
+    /*this.realRepos$.subscribe(realrepos => {
+      for (var i = 0; i < 6; i++) {
+        this.repos[i] = realrepos[i];
+      } //TODO: Star other repos and try again to see if maps correctly, if it does, then begin replacing other implementation
+        with the actual observable.
+    });*/
+    console.log(this.repos);
   }
 }
