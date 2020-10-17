b5faa59f96eaecd747809d7805490002b6041272
mattWStevens <gibsonhistoric@gmail.com>
2020-10-17 10:22:14 -0600

    comment

--- src/app/components/repo-detail/repo-detail.component.ts
+++ src/app/components/repo-detail/repo-detail.component.ts
@@ -11,17 +11,10 @@ import { PARAMETERS } from '@angular/core/src/util/decorators';
 })
 export class RepoDetailComponent implements OnInit {
   repo: Repo;
+  name: String = window.history.state['name'];
+  description: String = window.history.state['description'];
+  url: String = window.history.state['url'];
 
   constructor(private repoService: RepoService, private activatedRoute: ActivatedRoute) { }
-
-  ngOnInit() {
-    const repoList: Repo[] = this.repoService.getRepos();
-    //TODO: Make API pass in the github link or name into this class instead of repoId and make specific call here
-    // for that specific repo.
-
-    this.activatedRoute.paramMap.subscribe(params => {
-      this.repo = repoList[+params.get('repoId')];
-    });
-  }
 
 }
