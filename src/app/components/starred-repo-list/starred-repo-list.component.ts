4ba2f5bc7f6e7d1fb915c0fb81f87b075fe39f20
mattWStevens <gibsonhistoric@gmail.com>
2020-10-17 10:33:33 -0600

    Removed unused files.

--- src/app/components/starred-repo-list/starred-repo-list.component.ts
+++ src/app/components/starred-repo-list/starred-repo-list.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { RepoService } from '../../services/repo.service';
-import { Repo } from '../../repo';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { RepoResponse } from 'src/app/repo-response';
@@ -14,10 +11,9 @@ import { RepoResponse } from 'src/app/repo-response';
 export class StarredRepoListComponent implements OnInit {
   realRepos$: Observable<RepoResponse[]>;
 
-  constructor(private router: Router, private repoService: RepoService, private http: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
     this.realRepos$ = this.http.get<RepoResponse[]>('https://api.github.com/users/mattWStevens/starred');
-    //TODO: Move http get to service get method so can call that and assign to observable***
   }
 }
